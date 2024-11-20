1d4a0d08d0d5d53252a98d5172d1c6347b832ec8
Seohyun Lee <153355118+seohyun8825@users.noreply.github.com>
2024-11-20 14:28:16 +0900

    Update 0125-valid-palindrome.py

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,7 +5,6 @@ class Solution:
             if char.isalnum():
                 strs.append(char.lower())
 
-        # 팰린드롬 여부 판별
         while len(strs) > 1:
             if strs.pop(0) != strs.pop():
                 return False
