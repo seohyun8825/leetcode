24d66b442c0ab588565143732ccee91602485ffe
Seohyun Lee <153355118+seohyun8825@users.noreply.github.com>
2024-11-22 10:53:54 +0900

    Time: 11 ms (79.63%), Space: 20.1 MB (54.37%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,9 +2,9 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
         anagrams = defaultdict(list)
-
+        
         for word in strs:
-
+            # ['a', 'e', 't'] -> "aet"
             key = ''.join(sorted(word))
             anagrams[key].append(word)
 
