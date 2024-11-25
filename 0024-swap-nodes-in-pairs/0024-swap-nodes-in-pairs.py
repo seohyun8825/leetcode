fbc5dcb4d2d8319cfa8ce3ef3d90f2c2ac2f453a
Seohyun Lee <153355118+seohyun8825@users.noreply.github.com>
2024-11-26 08:53:37 +0900

    Time: 0 ms (100%), Space: 16.7 MB (6.75%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -3,6 +3,7 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+'''
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
         current = head
@@ -11,4 +12,13 @@ class Solution:
             current.val, current.next.val = current.next.val, current.val
             current = current.next.next
 
+        return head
+'''
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head and head.next:
+            point = head.next
+            head.next = self.swapPairs(point.next)
+            point.next = head
+            return point
         return head
